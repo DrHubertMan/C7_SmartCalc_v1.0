b23203fc59bc05907e48922431b7e8ac9f3f2af0
mammiemi_home <mammiemi@student.21-school.ru>
2023-02-12 19:36:22 +0300

    sort bug

--- src/main.c
+++ src/main.c
@@ -15,10 +15,10 @@ int char_is_operator(char sym);
 
 int main() {
   Node *stack = NULL;
-  char input[255] = "(2 + 4 * (12 + 14) / 2) + (13 * (4 + 6))";
+  char input[255] = "(2 + 4 * (8 + 9) / 2) + (7 * (4 + 6))";
   char output[255];
-  //sort(input, output, &stack);
-  //printf("%s\n", output);
+  sort(input, output, &stack);
+  printf("%s\n", output);
   return 0;
 }
 
@@ -50,12 +50,56 @@ int char_is_operator(char sym) {
   return (sym == 42 || sym == 43 || sym == 45 || sym == 47 || sym == 94) ? 1 : 0;
 }
 
+// * = 42
+// + = 43
+// - = 45
+// / = 47
+// ^ = 94
+
 void sort(char *input, char *output, Node **stack) {
-  for (int i = 0; i < 255; i++) {
+  int output_sym_counter = 0;
+  int input_leng = strlen(input);
+  for (int i = 0; i < input_leng; i++) {
     if (input[i] == ' ') {
       continue;
     } else if (input[i] == '(') {
+       push(stack, input[i]);
+    
+    } else if (char_is_operator(input[i])) {
+      while (view_last_char_in_stack(*stack) == 94) {
+        output[output_sym_counter] = pop(stack);
+        output_sym_counter++;
+      }
+
+      if (input[i] == 43 || input[i] == 45) {
+        while (char_is_operator(view_last_char_in_stack(*stack))) {
+          output[output_sym_counter] = pop(stack);
+          output_sym_counter++;
+        }
+      } else if (input[i] == 42 || input[i] == 47) {
+        while (view_last_char_in_stack(*stack) == input[i]) {
+          output[output_sym_counter] = pop(stack);
+          output_sym_counter++;
+        }
+      }
       push(stack, input[i]);
-    } else if (char_is_operator)
+    } else if (input[i] == ')') {
+      while (view_last_char_in_stack(*stack) != '(') {
+        if (char_is_operator(view_last_char_in_stack(*stack))) {
+          output[output_sym_counter] = pop(stack);
+          output_sym_counter++;
+        }
+      }
+      // if (view_last_char_in_stack(*stack) != ')') {
+      //   pop(stack);
+      // }
+    } else {
+      output[output_sym_counter] = input[i];
+      output_sym_counter++;
+    }
+  }
+  while ((*stack)->next) {
+    output[output_sym_counter] = pop(stack);
+    output_sym_counter++;
   }
 }
